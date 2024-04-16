d50d08d6c3b9c345e6496678a0139f0f1e6d736c
unknown <sknizm007@gmail.com>
2024-04-16 17:20:09 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Pages/Home/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
+import Navbar from './Components/Navbar/Navbar'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path='/' element={<Home/>}/>
       </Routes>
       <Footer/>
+      <Navbar/>
       </BrowserRouter>
     </div>
   )
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../Navbar/Navbar.css'
+import { CiHome } from "react-icons/ci";
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+    <div className="option">
+    <CiHome className='icons' />
+    </div>
+    <div className="option">
+    <CiHome className='icons' />
+    </div>
+    <div className="option">
+    <CiHome className='icons' />
+    </div>
+    <div className="option">
+    <CiHome className='icons' />
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
